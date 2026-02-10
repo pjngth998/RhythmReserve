c2668a53d00aff93f9b444498948106a4fb680c9
ธนัชญา พิทักษ์โกศล <yyaaone@Macintosh-3.local>
2026-02-10 15:28:54 +0700

    API Check_in_out

--- API/Checkin_out.py
+++ API/Checkin_out.py
@@ -1,12 +1,49 @@
-from typing import Union
+# from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+from classcode import ReserveSystem, User, FrontDeskStaff,TransStatus,Transaction,Room,TimeSlotStatus,TimeSlot
 import uvicorn
 
 app = FastAPI()
+system = ReserveSystem()
 
-@app.get("/")
-def read_root():
-    return {"Hello":"World"}
+mock_user = User("Yam","A101")
+system.add_user(mock_user)
+
+class CheckRequest(BaseModel):
+    user_id: str
+    timestamp: datetime
+
+@app.post("/check-in")
+def check_in(data: CheckRequest):
+    user = system.search_user(data.user_id)
+    if not user:
+        raise PermissionError
+    
+    trans = user.search_transaction(data.timestamp)
+    if not trans:
+        raise PermissionError
+    
+    if trans.do_checkin():
+        return "CheckIn Successfully"
+    else:
+        raise ValueError
+
+@app.post("/check-out")
+def check_out(data : CheckRequest):
+    user = system.search_user(data.user_id)
+    if not user:
+            raise PermissionError
+
+    trans = user.search_transaction(data.timestamp)
+    if not trans:
+        raise PermissionError
+
+    if trans.do_checkout():
+        return "CheckOut Successfully"
+    else:
+        raise ValueError
 
 if __name__ == "__main__":
-    uvicorn.run("Checkin_out:app",host = "127.0.0.1",port=8000, reload = True)
+    uvicorn.run("checkin_out:app",host = "127.0.0.1",port=8000, reload = True)
