3d1fb689ff3421b3e1e55a76a641f36bbfc0c57e
ธนัชญา พิทักษ์โกศล <yyaaone@Macintosh-3.local>
2026-02-25 14:08:02 +0700

    select_eq

--- API/CheckIN/checkin.py
+++ API/CheckIN/checkin.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,  HTTPException
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import date
+from datetime import time
 from classcode import *
 from fastapi.responses import RedirectResponse
 import uvicorn
@@ -8,17 +9,34 @@ import uvicorn
 app = FastAPI()
 system = ReserveSystem()
 
-mock_room = Room("A101", TimeSlotStatus.AVAILABLE)
+# mock_room = Room("A101", TimeSlotStatus.AVAILABLE)
 mock_ts = TimeSlot("2024-02-24", "10:00", "12:00",TimeSlotStatus.AVAILABLE)
-mock_room.add_time_slot(mock_ts)
 
 mock_cust = Customer("Yam", "C001","68010491")
 system.add_customer(mock_cust)
+
+mock_branch = Branch("Ladkrabang", "B001")
+system.add_branch(mock_branch)
+
+mock_room = Room("A101", "Medium", 500.0, 10) 
+mock_branch.add_room(mock_room)
+mock_room.add_time_slot(mock_ts)
+
 mock_res = Service_IN("A111",mock_cust)
-mock_booking = Booking("B1", "2024-02-24","10:00", "12:00", mock_cust, mock_room, "2024-02-23", "CONFIRMED", 500.0)
-mock_res.add_booking(mock_booking)
 
 mock_cust.add_reserve_list(mock_res)
+
+eq1 = Equipment("E01", 2, 20) # ขนาด 2, สต็อก 20
+eq2 = Equipment("E02", 4, 10) # ขนาด 4, สต็อก 10
+mock_branch.add_equipment(eq1)
+mock_branch.add_equipment(eq2)
+
+eq1.quantity = 2  
+eq2.quantity = 1  
+
+mock_eq_list = [eq1,eq2]
+mock_booking = Booking("B1", "2024-02-24","10:00", "12:00", mock_cust, mock_room, "2024-02-23", "CONFIRMED", 500.0,mock_eq_list)
+mock_res.add_booking(mock_booking)
 
 @app.get("/")
 def root():
@@ -28,6 +46,28 @@ def root():
 def api_checkin(customer_id: str, reserve_id: str):
     result = system.checkin(customer_id, reserve_id)
     if result != "CHECK-IN SUCCESSFULLY!":
+        raise HTTPException(status_code=404, detail=result)
+    return {"message": result}
+
+@app.post("/select_eq")
+def api_select_eq(customer_id : str, branch_id : str,room_id : str, eq_list : list[str], booking_id : str, date:date ,start_t:time ,end_t:time):
+    branch = system.search_branch(branch_id)
+    if not branch:
+        raise HTTPException(status_code=404, detail="Branch not found")
+    
+    actual_objects = []
+    for eid in eq_list:
+        for real_eq in branch.equipment_list:
+            if real_eq.eq_id == eid:
+                real_eq.quantity = 1 # กำหนดจำนวนพื้นฐาน
+                actual_objects.append(real_eq)
+                break
+
+    # 3. ส่งลิสต์ที่เป็น "Object" (ที่มี get_size) เข้าไปทำงาน
+    result = system.select_eq(customer_id, branch_id, room_id, 
+                              actual_objects, # <--- ต้องส่งตัวนี้เข้าไป!
+                              booking_id, date, start_t, end_t)
+    if result != "Can Reserve Equipment - Add to Booking Successfully":
         raise HTTPException(status_code=404, detail=result)
     return {"message": result}
 
--- API/CheckIN/classcode.py
+++ API/CheckIN/classcode.py
@@ -1,10 +1,11 @@
-import datetime
+from datetime import datetime
 from enum import Enum
 
 class ReserveSystem():
     def __init__(self):
         self.__customer_list = [] 
         self.__staff_list = []
+        self.__branch_list =[]
         # self.__room_list = [Room("A101"),Room("B202"),Room("C303")]
 
     def search_customer(self,customer_id):
@@ -16,6 +17,8 @@ class ReserveSystem():
     def add_customer(self,customer):
         self.__customer_list.append(customer)
     
+    def add_branch(self,branch):
+        self.__branch_list.append(branch)
 
     def checkin(self,customer_id,reserve_id):
         customer = self.search_customer(customer_id)
@@ -30,6 +33,89 @@ class ReserveSystem():
         if success:
             return "CHECK-IN SUCCESSFULLY!"
         
+
+    def search_branch(self,branch_id):
+        for branch in self.__branch_list:
+            if branch.branch_id == branch_id:
+                return branch
+        return None
+
+    def create_booking(self,booking_id,date,start_tBooking,end_tBooking,customer,room,created_at, status, price, eq_list):
+        return Booking( booking_id, date, start_tBooking, end_tBooking, customer, room, created_at, status, price, eq_list)
+    
+    def select_eq(self,customer_id,branch_id,room_id,eq_list,booking_id,date,start_t,end_t):
+        branch = self.search_branch(branch_id)
+        
+        customer = self.search_customer(customer_id)
+        customer.get_customer_info(customer_id)
+
+        room = branch.search_room(room_id)
+        max_quota = room.get_eq_quota(room_id)
+
+        total_requested_size = 0
+
+        created_at = datetime.now().strftime("%Y-%m-%d %H:%M") 
+        status = "PENDING"
+        price = room.rate
+
+        for eq in eq_list:
+            if not branch.check_stock_eq(eq.id, eq.quantity):
+                return "Not Enough for Reserve"
+            
+            size = eq.get_size()
+
+            total_size = eq.calculate_quota(eq.quantity)
+            total_requested_size += total_size
+
+        if total_requested_size <= max_quota:
+            c_booking = self.create_booking(booking_id,date, start_t, end_t, customer, room, created_at, status,price,eq_list)
+            # c_booking.add_eq_list(customer, eq_list)
+            customer
+            return "Can Reserve Equipment - Add to Booking Successfully"
+        else:
+            return "Exceed Room Quota Limit"
+        
+class Branch():
+    def __init__(self,name,id):
+        self.__name = name
+        self.__branch_id = id
+        self.__room_list = []
+        self.__equipment_list = [] 
+
+    @property
+    def branch_id(self):
+        return self.__branch_id
+    
+    @property
+    def room_list(self):
+        return self.__room_list
+    
+    @property
+    def equipment_list(self):
+        return self.__equipment_list
+    
+    def add_room(self,room):
+        self.__room_list.append(room)
+
+    def add_equipment(self,eq):
+        self.__equipment_list.append(eq)
+
+    def get_branch_info(self):
+        return self.__name, self.__room_list, self.__equipment_list
+    
+    def search_room(self,room_id):
+        for room in self.__room_list:
+            if room.room_id == room_id:
+                return room
+        return None
+
+
+    def check_stock_eq(self,eq_id,requested_qty):
+        for eq in self.__equipment_list:
+            if eq.eq_id == eq_id: # เช็คจาก ID
+                return eq.stock_qty >= requested_qty # คืนค่า True/False
+        return False
+    
 class User():
     def __init__(self,name,id):
         self.__username = name
@@ -61,6 +147,10 @@ class Customer(User):
     def reserve_list(self):
         return self.__reserve_list
     
+    def get_customer_info(self,customer_id):
+        if self.__customer_id == customer_id:
+            return self.__customer_name, self.__customer_id,self.__reserve_list
+        
     def add_reserve_list(self,reserve):
         self.__reserve_list.append(reserve)
     
@@ -100,8 +190,9 @@ class Service_IN:
 
 
 class Booking:
-    def __init__(self,id,date,start_tBooking,end_tBooking,customer,room,created_at,status,price):
+    def __init__(self,id,date,start_tBooking,end_tBooking,customer,room,created_at,status,price,eq_list):
         self.__booking_id = id
+        self.__equipment_list =eq_list
         self.__date =date
         self.__ST_booking = start_tBooking
         self.__ED_booking = end_tBooking
@@ -110,7 +201,7 @@ class Booking:
         self.__create_at = created_at
         self.__status = status
         self.__price = price
-        self.__equipment_list =[]
+    
     
     @property
     def booking_id(self):
@@ -124,13 +215,29 @@ class Booking:
 
     
 class Room:
-    def __init__(self,room_id,status):
+    def __init__(self,room_id,size,rate,eq_quota):
         self.__room_id = room_id
+        self.__size = size
+        self.__rate = rate
+        self.__eq_quota = eq_quota
         self.__time_slot =[]
+        
 
     @property
     def room_id(self):
-        self.__room_id
+        return self.__room_id
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @property
+    def rate(self):
+        return self.__rate
+
+    @property
+    def eq_quota(self):
+        return self.__eq_quota
     
     def add_time_slot(self,time):
         self.__time_slot.append(time)
@@ -142,17 +249,40 @@ class Room:
                 return "update status 'OCCUPIED' success"
         return False
 
+    def get_eq_quota(self,room_id):
+        if self.__room_id == room_id:
+            return self.__eq_quota
+        return None
+    
 class Equipment:
-    def __init__(self,id):
+    def __init__(self,id,size,stock):
         self.__eq_id = id
+        self.__size = size
+        self.__stock_qty= stock
         self.__time_slot = []
+        self.__quantity =  0
     
     @property
     def eq_id(self):
         return self.__eq_id
     
+    @property
+    def stock_qty(self):
+        return self.__stock_qty
+    
+    @property
+    def quantity(self):
+        return self.__quantity
+    
+    @quantity.setter
+    def quantity(self, value):
+        self.__quantity = value
+    
     def add_time(self,time):
         self.__time_slot.append(time)
+
+    def calculate_quota(self, qty):
+        return self.__size * qty 
     
     def get_equipment_reserve_detail(self,date,ST_time,ED_time):
         for time_slot in self.__time_slot:
@@ -160,6 +290,11 @@ class Equipment:
                 time_slot.update_status()
                 return "update status 'OCCUPIED' success"
         return False
+    
+    def get_size(self):
+        return self.__size
+    
+
 
 class TimeSlotStatus(Enum):
     AVAILABLE = "Available"
@@ -193,8 +328,6 @@ class TimeSlot:
             self.__status = TimeSlotStatus.OCCUPIED
             return True
         return False
-
-
     
     def reserve(self):
         if self.__status == TimeSlotStatus.AVAILABLE:
